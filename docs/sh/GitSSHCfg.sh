709169e2492bffb99b47f04a1cf2e0b2794508ce
renlm <renlm@21cn.com>
2024-01-27 17:14:02 +0800

    up

--- docs/sh/GitSSHCfg.sh
+++ docs/sh/GitSSHCfg.sh
@@ -3,7 +3,7 @@ set -e
 set -o noglob
 if [ -z "${HOST}" ] || [ -z "${ALIAS}" ] || [ -z "${FILE}" ]; then
 	echo "tips: curl -sfL https://renlm.gitee.io/docs/sh/GitSSHCfg.sh | HOST={host} ALIAS={alias} FILE={file} sh"
-elif [ ! -s ~/.ssh/${FILE} ]; then
+elif [ ! -s ${FILE} ]; then
 	echo "tips: ssh-keygen -t ed25519 -C "\"${ALIAS}@${HOST}\"" -f ${FILE}"
 else
 tee -a ~/.ssh/config <<-EOF
