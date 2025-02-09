f3b9cc641b7ddb68e6cbbd8596869136ea8f64fe
renlm-a <renlm-a@glodon.com>
2025-02-09 18:01:25 +0800

    up

--- script/docker/install.sh
+++ script/docker/install.sh
@@ -8,12 +8,15 @@ DATA_ROOT=${@:-'/home/docker'}
 REGISTRY_MIRRORS=${REGISTRY_MIRRORS:-'https://docker.1ms.run'}
 
 # 操作系统
-system=`lsb_release -i | cut -f 2`
-echo "The system is $system."
+OS_ID=`cat /etc/os-release | grep ^ID= | cut -d = -f 2 | tr -d '"'`
+OS_VERSION=`cat /etc/os-release | grep ^VERSION_ID= | cut -d = -f 2 | tr -d '"'`
+echo "The system is $OS_ID $OS_VERSION."
  
 # 安装脚本
-if [ "$system" = "Ubuntu" ]; then
+if [ "$OS_ID" = "ubuntu" ]; then
 	curl -sfL https://github-io.renlm.cn/script/docker/install/ubuntu.sh | REGISTRY_MIRRORS=$REGISTRY_MIRRORS bash -s $DATA_ROOT
+elif [ "$OS_ID" = "rhel" ]; then
+	curl -sfL https://github-io.renlm.cn/script/docker/install/rhel.sh | REGISTRY_MIRRORS=$REGISTRY_MIRRORS bash -s $DATA_ROOT
 else
 	echo "Does not support automatic installation of Docker."
 fi
---
+++ script/docker/install/rhel.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+set -o noglob
+
+# 参数
+DATA_ROOT=${@}
+REGISTRY_MIRRORS=${REGISTRY_MIRRORS}
+
+# 镜像源
+# https://developer.aliyun.com/mirror/docker-ce
+OS_MAIN_VERSION=`cat /etc/os-release | grep ^VERSION_ID= | cut -d = -f 2 | tr -d '"' | cut -d . -f 1`
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo \
+  && wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-${OS_MAIN_VERSION}.repo \
+  && sed -i 's/\$releasever/'${OS_MAIN_VERSION}'/g' /etc/yum.repos.d/docker-ce.repo \
+  && sed -i 's/\$releasever/'${OS_MAIN_VERSION}'/g' /etc/yum.repos.d/CentOS-Base.repo \
+  && yum clean all \
+  && yum makecache
+  
+# 安装
+# https://docs.docker.com/engine/install/rhel/
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+if [ ! grep -q 'alias docker-compose="docker compose"' ~/.bashrc ]; then
+	sed -i '$a alias docker-compose="docker compose"' ~/.bashrc
+	source ~/.bashrc
+fi
+
+# 配置
+cat <<EOF | tee /etc/docker/daemon.json
+{
+  "data-root": "$DATA_ROOT",
+  "features": { "buildkit" : true },
+  "exec-opts": [ "native.cgroupdriver=systemd" ],
+  "registry-mirrors": [ "$REGISTRY_MIRRORS" ],
+  "log-driver": "json-file",
+  "log-opts": { "max-size": "100m", "max-file": "3" }
+}
+EOF
+
+# 重启
+systemctl daemon-reload
+systemctl enable docker
+systemctl restart docker
