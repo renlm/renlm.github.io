5380e10708772517e2df03d6b3aa68379ce13e7b
黎明时分 <renlm-a@glodon.com>
2026-06-19 22:28:35 +0800

    helm: v4.0.5

--- sh/k3s-install.sh
+++ sh/k3s-install.sh
@@ -9,9 +9,9 @@ set -o noglob
 # https://helm.sh/docs/topics/version_skew/
 # https://github.com/k3s-io/k3s/releases
 # https://github.com/helm/helm/releases/
-# [ 版本匹配 ] k3s: v1.34.8+k3s1, helm: v4.1.4, rancher: v2.14.2
+# [ 版本匹配 ] k3s: v1.34.8+k3s1, helm: v4.0.5, rancher: v2.14.2
 INSTALL_K3S_VERSION=${INSTALL_K3S_VERSION:-"v1.34.8+k3s1"}
-INSTALL_HELM_VERSION=${INSTALL_HELM_VERSION:-"v4.1.4"}
+INSTALL_HELM_VERSION=${INSTALL_HELM_VERSION:-"v4.0.5"}
 DOWNLOAD_K3S_VERSION=$(echo ${INSTALL_K3S_VERSION} | sed "s/+/-/g")
 DOWNLOADER_URL=${DOWNLOADER_URL:-"https://obs.renlm.cn"}
 ###### master 主节点
