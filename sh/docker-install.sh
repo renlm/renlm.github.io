feb917bc203c0e7d6ef3df9a9fd7ddc0e2cd4a8f
黎明时分 <renlm-a@glodon.com>
2026-06-20 20:02:49 +0800

    up

--- sh/docker-install.sh
+++ sh/docker-install.sh
@@ -155,12 +155,15 @@ create_service() {
   DOCKER_SOCKET_FILE="/etc/systemd/system/docker.socket"
   DOCKER_CONTAINERD_FILE="/etc/systemd/system/containerd.service"
   printf "[ ${_GREEN_}开机自启${_NC_} ] ${DOCKER_SERVICE_FILE}\n"
+  mkdir -p ${DOCKER_CONFIG%/*}
   touch ${DOCKER_SERVICE_FILE}
   touch ${DOCKER_SOCKET_FILE}
   touch ${DOCKER_CONTAINERD_FILE}
+  touch ${DOCKER_CONFIG}
   chmod 0755 ${DOCKER_SERVICE_FILE}
   chmod 0755 ${DOCKER_SOCKET_FILE}
   chmod 0755 ${DOCKER_CONTAINERD_FILE}
+  chmod 0600 ${DOCKER_CONFIG}
   cat <<EOF | tee ${DOCKER_SERVICE_FILE} >/dev/null
 [Unit]
 Description=Docker Application Container Engine
@@ -173,6 +176,7 @@ StartLimitIntervalSec=60
 
 [Service]
 Type=notify
+Environment="DOCKER_CONFIG=${DOCKER_CONFIG}"
 # the default is not to use systemd for cgroups because the delegate issues still
 # exists and systemd currently does not support the cgroup feature set required
 # for containers run by docker
@@ -262,7 +266,7 @@ OOMScoreAdjust=-999
 WantedBy=multi-user.target
 
 EOF
-  cat <<EOF | tee /etc/docker/daemon.json >/dev/null
+  cat <<EOF | tee ${DOCKER_CONFIG} >/dev/null
 {
   "data-root": "$DOCKER_DATA_ROOT",
   "features": { "buildkit" : true },
@@ -283,6 +287,7 @@ EOF
 # [ aarch64 | x86_64 ]
 INSTALL_DOCKER_ROOT=/usr/local/bin
 INSTALL_DOCKER_BIN=${INSTALL_DOCKER_ROOT}/docker
+DOCKER_CONFIG=/etc/docker/daemon.json
 DOWNLOADS_ROOT=/opt/docker-install
 DOWNLOADER=curl
 # 下载并安装
