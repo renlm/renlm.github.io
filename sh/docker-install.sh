003139fc1c4fc956cd82f80cb7d3035d8e14f055
黎明时分 <renlm-a@glodon.com>
2026-06-21 13:00:55 +0800

    up

--- sh/docker-install.sh
+++ sh/docker-install.sh
@@ -30,6 +30,21 @@ _GREEN_='\033[0;32m'  # 绿色
 _YELLOW_='\033[0;33m' # 黄色
 _NC_='\033[0m'        # 重置
 
+# --- helper functions for logs ---
+info()
+{
+    echo '[INFO] ' "$@"
+}
+warn()
+{
+    echo '[WARN] ' "$@" >&2
+}
+fatal()
+{
+    echo '[ERROR] ' "$@" >&2
+    exit 1
+}
+
 # 内核参数调整
 kernel_parameter_adjustment() {
   SYSCTL_P=0
